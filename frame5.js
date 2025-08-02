164ed1603e8bd0d302d5bbef838eed29cfa3af60
priyansh2304 <160208576+priyansh2304@users.noreply.github.com>
2025-08-02 06:09:29 +0530

    Update frame5.js

--- frame5.js
+++ frame5.js
@@ -5,15 +5,17 @@ const nextBtn = document.getElementById('toFrame6');
 
 // ✍️ Your letter goes here:
 const letter = `
-Dear [Bestie’s Name],
+Dear Pihuuuuu,
 
-I just wanted to take a moment to say... you're one of the most special people in my life. 🥺💖
+You are the most precious gift of god for me. I'm incredibly grateful to have you as my best friend. 
+You're the person who always makes my day better, understands me without needing to explain. 
+I'm so grateful to have a bestie like you, thank you so much for being my bestfriend and filling my life with happiness. 🥺💖
 This little surprise is just a tiny piece of the happiness you deserve today.
 
 So smile big — because this day is all about YOU. 🎉
 
 With all my heart,
-[Your Name]
+Your Prinshuuuuuu 
 `;
 
 // Typing effect function
@@ -36,3 +38,4 @@ envelope.addEventListener('click', () => {
 nextBtn.addEventListener('click', () => {
   window.location.href = "frame6.html"; // Change this if your frame6 is named differently
 });
+
